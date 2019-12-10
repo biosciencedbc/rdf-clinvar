874e9bfb36af3665d6704461f2020b7f34943429
ytokunaga-hi <togovar@nbdc709.localhost>
2019-12-10 09:44:01 +0900

    update

--- lib/clinvar/rdf/version.rb
+++
@@ -1,5 +0,0 @@
-module ClinVar
-  module RDF
-    VERSION = '0.1.0'.freeze
-  end
-end
--- spec/clinvar_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe ClinVar::RDF do
-  it 'has a version number' do
-    expect(ClinVar::RDF::VERSION).not_to be nil
-  end
-end
